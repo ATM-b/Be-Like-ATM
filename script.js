4b9ab5e33330c38ca70a530832fb9c65052e9c53
ATM-b <temitopemichael619@gmail.com>
2025-07-28 19:39:20 -0700

    updated

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".Menu");
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+});
+ 
